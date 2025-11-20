8354c28e8b1268614784013654bb4d7cfd44a581
ferrumyanis <ferrumyanis81@gmail.com>
2025-11-19 23:32:30 -0800

    Add workerMode configuration to medusa-config

--- medusa-config.ts
+++ medusa-config.ts
@@ -11,6 +11,12 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+  },
+   admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
     }
   }
 })
